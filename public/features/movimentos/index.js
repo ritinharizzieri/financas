5bbb6c0cc76c8d538ca6359d9f1f15162061c1b5
Rita Rizzieri <ritarizzieri@gmail.com>
2021-02-15 19:00:44 -0300

    features/movimentos: criado component html de conta

--- public/features/movimentos/index.js
+++ public/features/movimentos/index.js
@@ -5,7 +5,7 @@ function ComponentMenu() {
     { href: "/contas", text: "contas", active: false },
   ];
 
-  this.menuItemsList = menuItems.map((item) => {
+  this.menuItemsList = this.menuItems.map((item) => {
     return new MenuItemComponent(item.href, item.text, item.active).render();
   });
 
@@ -15,12 +15,34 @@ function ComponentMenu() {
   };
 }
 
+function ComponentAccount() {
+  this.accountItems = [
+    { name: "Nubank", balance: 100.0 },
+    { name: "Itau", balance: 80.0 },
+    { name: "Carteira", balance: 30.0 },
+    { name: "Santander", balance: 50.0 },
+  ];
+
+  this.accountListItems = this.accountItems.map((item) => {
+    return new AccountListItemComponent(item.name, item.balance).render();
+  });
+
+  this.render = () => {
+    this.items = this.accountListItems;
+    return `<div class="account-list"> ${this.items.map(
+      (item) => item
+    )} </div>`;
+  };
+}
+
 function MovimentosFeature() {
   this.root = document.createElement("div");
 
   this.render = () => {
     this.root.innerHTML = `
-    ${new ComponentMenu().render()}`;
+    ${new ComponentMenu().render()}
+    ${new ComponentAccount().render()}
+    `;
 
     return this.root.innerHTML;
   };
