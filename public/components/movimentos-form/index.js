08189ea992a51ef85a8b29043fd2380b194a7098
Rita Rizzieri <ritarizzieri@gmail.com>
2021-02-27 12:42:19 -0300

    ajustando componente de movimento-form

--- public/components/movimentos-form/index.js
+++ public/components/movimentos-form/index.js
@@ -1,22 +1,79 @@
-function MovimentosFormButtonsComponent(type = "") {
-  this.type = type;
+function MovimentosFormComponent(rootEl) {
+  this.rootEl = rootEl;
+  this.el = null;
 
-  this.render = () => {
-    return `    
-      <button type="button" class="movimento-form__button movimento-form__button--${
-        this.type == "Débito" ? "debit" : "credit"
-      }">
-        ${this.type}
-      </button>      
-    `;
+  this.title = "";
+  this.value = "";
+
+  this.onDebitClick = () => {};
+  this.onCreditClick = () => {};
+
+  this.setTitle = (newTitle) => {
+    this.title = newTitle;
+    this.update();
   };
-}
 
-function MovimentosFormComponent(items) {
-  this.items = items;
+  this.setValue = (value) => {
+    this.value = value;
+    this.update();
+  };
+
+  /*
+   * bindListeners()
+   * Cria eventListener para elementos internos
+   */
+  this.bindListeners = () => {
+    this.el.querySelector(".js-button-debit").addEventListener("click", () => {
+      this.onDebitClick(this.value);
+      this.setValue("");
+    });
+
+    this.el.querySelector(".js-button-credit").addEventListener("click", () => {
+      this.onCreditClick(this.value);
+      this.setValue("");
+    });
+    this.el.querySelector(".js-input").addEventListener("input", (event) => {
+      this.value = +event.target.value;
+    });
+  };
+
+  /*
+   * render()
+   * Atualiza o html do component
+   */
   this.render = () => {
-    `<div class="movimento-form__buttons">
-        ${this.items.map((item) => item.render())}
-      </div>`;
+    this.el.innerHTML = `
+      <div class="movimento-form">
+        <h3 class="movimento-form__title">${this.title}</h3>
+        <div class="form-field">
+          <label class="form-field__label">Valor</label>
+          <input type="text" value="${this.value}" class="form-field__input js-input">
+        </div>
+        <div class="movimento-form__buttons">
+          <button type="button" class="movimento-form__button movimento-form__button--debit js-button-debit">Débito</button>
+          <button type="button" class="movimento-form__button movimento-form__button--credit js-button-credit">Crédito</button>
+        </div>
+      </div>
+    `;
+    this.rootEl.innerHTML = "";
+    this.rootEl.appendChild(this.el);
+  };
+
+  /*
+   * update()
+   * Executa o render() e o bindListeners()
+   */
+  this.update = () => {
+    this.render();
+    this.bindListeners();
+  };
+
+  /*
+   * init()
+   * Executa o update() pela primeira vez
+   */
+  this.init = () => {
+    this.el = document.createElement("div");
+    this.update();
   };
 }
--- public/scripts.js
+++ public/scripts.js
@@ -1,64 +1,16 @@
-// Menu
-const menuItems = [
-  { href: "/movimentos", text: "Movimentos", active: false },
-  { href: "/categorias", text: "categorias", active: false },
-  { href: "/contas", text: "contas", active: false },
-];
-const menuItemsList = menuItems.map((item) => {
-  return new MenuItemComponent(item.href, item.text, item.active);
-});
+const rootEl = document.querySelector("#root");
 
-// Moviment
-const movimentosItems = [
-  {
-    type: "Despesa",
-    typeClass: "debit",
-    value: 30.0,
-    description: "lorem ipsum dolor 1",
-  },
-  {
-    type: "Despesa",
-    typeClass: "debit",
-    value: 20.0,
-    description: "lorem ipsum dolor 2",
-  },
-  {
-    type: "Receita",
-    typeClass: "credit",
-    value: 10.0,
-    description: "lorem ipsum dolor 3",
-  },
-];
-const movimentosItemsList = movimentosItems.map((item) => {
-  return new MovimentosItemComponent(
-    item.type,
-    item.typeClass,
-    item.value,
-    item.description
-  );
-});
+// criar instancia
+const movimentosForm = new MovimentosFormComponent(rootEl);
 
-// Account
-const accountItems = [
-  { name: "Nubank", balance: 100.0 },
-  { name: "Itau", balance: 80.0 },
-  { name: "Carteira", balance: 30.0 },
-  { name: "Santander", balance: 50.0 },
-];
-const accountListItems = accountItems.map((item) => {
-  return new AccountListItemComponent(item.name, item.balance);
-});
+movimentosForm.init();
+movimentosForm.setTitle("Novo movimento");
+console.log(movimentosForm);
 
-// Category
-const categoryItems = [
-  { name: "Alimentação" },
-  { name: "Mercado" },
-  { name: "Transporte" },
-  { name: "Faculdade" },
-];
-const categoryListItems = categoryItems.map((item) => {
-  return new CategoryListItemComponent(item.name);
-});
+movimentosForm.onCreditClick = (valorCredito) => {
+  console.log(`adicionou um credito de R$ ${valorCredito}`);
+};
 
-//console.log(categoryListItems);
-console.log("MovimentosFeature", new MovimentosFeature().render());
+movimentosForm.onDebitClick = (valorDebito) => {
+  console.log(`adicionou um debito de R$ ${valorDebito}`);
+};
