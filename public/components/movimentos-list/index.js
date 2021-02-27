6db2783e90a34632e74613934b6ce6774292f7e8
Rita Rizzieri <ritarizzieri@gmail.com>
2021-02-27 16:52:03 -0300

    criando componente movimentos lista e unindo os dois componentes

--- public/components/movimentos-list/index.js
+++ public/components/movimentos-list/index.js
@@ -1,3 +1,48 @@
+function MovimentosListComponent(rootEl) {
+  this.rootEl = rootEl;
+  this.el = null;
+  this.type = "";
+  this.value = "";
+
+  this.setItem = (type, value) => {
+    this.type = type;
+    this.value = value;
+  };
+
+  this.bindListeners = () => {
+    this.el.querySelector(".js-remove-item").addEventListener("click", () => {
+      this.el.querySelector(".js-remove-item").parentElement.remove();
+    });
+  };
+
+  this.render = () => {
+    this.el.innerHTML = `
+    <div class="movimentos-list">
+      <div class="movimentos-list__item js-movimento-list">
+        <div class="movimento">
+          <div class="movimento__type movimento__type--${this.type}">${this.type}</div>
+          <div class="movimento__value">${this.value}</div>
+          <div class="movimento__description">lorem ipsum dolor</div>          
+        </div>
+        <div class="movimento__remove js-remove-item">Remover</div>
+      </div>
+    </div>
+    `;
+    this.rootEl.appendChild(this.el);
+  };
+
+  this.update = () => {
+    this.render();
+    this.bindListeners();
+  };
+
+  this.init = () => {
+    this.el = document.createElement("div");
+    this.update();
+  };
+}
+
+/*
 function MovimentosItemComponent(
   id = number,
   type = "",
@@ -31,3 +76,4 @@ function MovimentosListComponent(items) {
     </div>`;
   };
 }
+*/
--- public/scripts.js
+++ public/scripts.js
@@ -1,14 +1,20 @@
+// componente movimentos-form
 const rootEl = document.querySelector("#root");
-
-// criar instancia
 const movimentosForm = new MovimentosFormComponent(rootEl);
 
 movimentosForm.init();
 movimentosForm.setTitle("Novo movimento");
 console.log(movimentosForm);
 
+// componente movimentos-list
+const rootList = document.querySelector("#root-list");
+const movimentosList = new MovimentosListComponent(rootList);
+
 movimentosForm.onCreditClick = (valorCredito) => {
   console.log(`adicionou um credito de R$ ${valorCredito}`);
+  movimentosList.setItem("credito", valorCredito);
+  movimentosList.init();
+  console.log(movimentosList);
 };
 
 movimentosForm.onDebitClick = (valorDebito) => {
